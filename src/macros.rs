1a66c23a5d6587a6e7bcd2277bc8192ccfcf8639
Danilo Bargen <mail@dbrgn.ch>
2016-11-20 17:40:07 +0100

    Add CenterLinePrimitive

--- src/macros.rs
+++ src/macros.rs
@@ -12,14 +12,16 @@ pub enum Primitive {
     Comment(String),
     Circle(CirclePrimitive),
     VectorLine(VectorLinePrimitive),
+    CenterLine(CenterLinePrimitive),
 }
 
 impl GerberCode for Primitive {
     fn to_code(&self) -> GerberResult<String> {
         let code = match *self {
             Primitive::Comment(ref s) => s.clone(),
-            Primitive::Circle(ref cp) => try!(cp.to_code()),
-            Primitive::VectorLine(ref cp) => try!(cp.to_code()),
+            Primitive::Circle(ref c) => try!(c.to_code()),
+            Primitive::VectorLine(ref vl) => try!(vl.to_code()),
+            Primitive::CenterLine(ref cl) => try!(cl.to_code()),
         };
         Ok(code)
     }
@@ -33,7 +35,7 @@ pub struct CirclePrimitive {
     /// Diameter, a decimal >= 0
     pub diameter: f64,
 
-    /// X and Y coordinates of center position, a decimal
+    /// X and Y coordinates of center position, decimals
     pub center: (f64, f64),
 
     /// Rotation angle.
@@ -67,10 +69,10 @@ pub struct VectorLinePrimitive {
     /// Line width, a decimal >= 0
     pub width: f64,
 
-    /// Coordinates of start point, a decimal
+    /// X and Y coordinates of start point, decimals
     pub start: (f64, f64),
 
-    /// Coordinates of end point, a decimal
+    /// X and Y coordinates of end point, decimals
     pub end: (f64, f64),
 
     /// Rotation angle of the vector line primitive
@@ -88,6 +90,38 @@ impl GerberCode for VectorLinePrimitive {
             try!(self.exposure.to_code()),
             self.width,
             self.start.0, self.start.1, self.end.0, self.end.1,
+            self.angle
+        );
+        Ok(code)
+    }
+}
+
+#[derive(Debug)]
+pub struct CenterLinePrimitive {
+    /// Exposure off/on (0/1)
+    pub exposure: bool,
+
+    /// Rectangle dimensions (width/height)
+    pub dimensions: (f64, f64),
+
+    /// X and Y coordinates of center point, decimals
+    pub center: (f64, f64),
+
+    /// Rotation angle
+    /// 
+    /// The rotation angle is specified by a decimal, in degrees. The primitive
+    /// is rotated around the origin of the macro definition, i.e. (0, 0) point
+    /// of macro coordinates.
+    pub angle: f64,
+}
+
+impl GerberCode for CenterLinePrimitive {
+    fn to_code(&self) -> GerberResult<String> {
+        let code = format!(
+            "21,{},{},{},{},{},{}",
+            try!(self.exposure.to_code()),
+            self.dimensions.0, self.dimensions.1,
+            self.center.0, self.center.1,
             self.angle
         );
         Ok(code)
@@ -128,5 +162,16 @@ mod test {
             angle: 0.,
         };
         assert_eq!(line.to_code().unwrap(), "20,1,0.9,0,0.45,12,0.45,0".to_string());
+    }
+
+    #[test]
+    fn test_center_line_primitive_codegen() {
+        let line = CenterLinePrimitive {
+            exposure: true,
+            dimensions: (6.8, 1.2),
+            center: (3.4, 0.6),
+            angle: 30.0,
+        };
+        assert_eq!(line.to_code().unwrap(), "21,1,6.8,1.2,3.4,0.6,30".to_string());
     }
 }
