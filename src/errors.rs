6f3be75b5296df1fd5308bc6eb4006762cc40598
Danilo Bargen <mail@dbrgn.ch>
2017-06-10 14:26:27 +0200

    Remove Clone from GerberError
    Clone is not implemented for `io::Error`.

--- src/errors.rs
+++ src/errors.rs
@@ -3,7 +3,7 @@
 use std::io::Error as IoError;
 
 quick_error! {
-    #[derive(Debug, Clone)]
+    #[derive(Debug)]
     pub enum GerberError {
         /// Conversion between two types failed
         ConversionError(msg: String) {}
