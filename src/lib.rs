ce97b2f8a6ca1832f8a138ae101377c392865757
Danilo Bargen <mail@dbrgn.ch>
2021-01-06 15:41:21 +0100

    Silence new_without_default clippy warning

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,8 @@
 //!   terminated with a newline character.
 //! - `PartialGerberCode` (internal only) generates Gerber representation of a
 //!   value, but does not represent a full line of code.
+#![allow(clippy::new_without_default)]
+
 #[cfg(test)]
 #[macro_use]
 mod test_macros;
